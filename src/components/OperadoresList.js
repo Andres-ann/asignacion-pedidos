73209377ca5ecab957e5d5106b5c8b2bfaf92560
Andres Negrette <andresdngtt@gmail.com>
2023-10-24 23:40:47 -0300

    Correccion de estilos en OperadoresList

--- src/components/OperadoresList.js
+++ src/components/OperadoresList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import operadoresData from '../data/operadores.json';
 
-function OperadoresList({ uniqueOperadores, operadoresSum }) {
+function OperadoresList({ operadoresSum }) {
   return (
     <div className="card shadow p-4 mb-4">
       <h4 className="text-center">Operadores</h4>
-      <div className="d-flex flex-wrap justify-content-center mt-2">
+      <div className="d-flex flex-wrap mt-2">
         {operadoresData.map((operadorInfo, index) => (
-          <div className="card card-body shadow me-2 mb-2" key={index}>
+          <div
+            className="card btn shadow p-3 me-2 mb-2 border-0 text-muted"
+            key={index}
+          >
             <div>
               {`${operadorInfo.operador} - ${operadorInfo.nombre}`}
               <span className="badge text-bg-warning ms-1">
